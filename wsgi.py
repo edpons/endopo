595b9773ee096c1bfd03fd0be8b2de796ebf7a92
edpons <dum3.w3c@gmail.com>
2019-01-31 13:51:28 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -25,7 +25,7 @@ def hotels():
 	
 	url='https://api.test.hotelbeds.com/hotel-content-api/1.0/hotels/2?language=CAT&useSecondaryLanguage=false'
 	
-	resp = Response(url=url, headers=request.headers=headers)
+	resp = Response(url=url, headers=headers)
 	resp.headers['Access-Control-Allow-Origin'] = '*'
 	return resp
 
