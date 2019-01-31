7e62509e89480fbb368a808c63179a47cc241f85
edpons <dum3.w3c@gmail.com>
2019-01-31 12:05:44 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,28 @@
 from flask import Flask
-application = Flask(__name__)
+import requests
+import time, hashlib
 
-@application.route("/")
-def hello():
-    return "Hello World!"
+app = Flask(__name__)
+
+api_key='emnrkrz4eetfaz6mgw7d5976'
+secret='RPPZsj4629'
+sigStr = "%s%s%d" % (api_key,secret,int(time.time()))
+signature = hashlib.sha256(sigStr.encode('utf-8')).hexdigest()
+
+headers={
+'Api-Key': api_key, 
+'X-Signature': signature
+}
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/aa/<id_hotel>')
+def hotels(id_hotel):
+	url='https://api.test.hotelbeds.com/hotel-content-api/1.0/hotels/<id_hotel>?language=CAT&useSecondaryLanguage=false'
+	r = requests.get(url, headers=headers)
+	return r.text
 
 if __name__ == "__main__":
     application.run()
