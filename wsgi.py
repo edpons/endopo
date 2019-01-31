b1e72551247e301251a550049821d31d19b40476
edpons <dum3.w3c@gmail.com>
2019-01-31 12:41:17 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -4,15 +4,7 @@ import time, hashlib
 
 application = Flask(__name__)
 
-api_key='emnrkrz4eetfaz6mgw7d5976'
-secret='RPPZsj4629'
-sigStr = "%s%s%d" % (api_key,secret,int(time.time()))
-signature = hashlib.sha256(sigStr.encode('utf-8')).hexdigest()
 
-headers={
-'Api-Key': api_key, 
-'X-Signature': signature
-}
 
 @application.route('/')
 def hello_world():
@@ -20,6 +12,16 @@ def hello_world():
 
 @application.route('/aa')
 def hotels():
+	api_key='emnrkrz4eetfaz6mgw7d5976'
+	secret='RPPZsj4629'
+	sigStr = "%s%s%d" % (api_key,secret,int(time.time()))
+	signature = hashlib.sha256(sigStr.encode('utf-8')).hexdigest()
+
+	headers={
+		'Api-Key': api_key, 
+		'X-Signature': signature
+	}
+
 	url='https://api.test.hotelbeds.com/hotel-content-api/1.0/hotels/2?language=CAT&useSecondaryLanguage=false'
 	r = requests.get(url, headers=headers)
 	r.headers['Access-Control-Allow-Origin'] = '*'
