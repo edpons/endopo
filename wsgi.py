75dc499fc45be5867e83870504c5b21bba3da93a
edpons <dum3.w3c@gmail.com>
2019-02-06 11:54:16 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -10,12 +10,12 @@ secret='RPPZsj4629'
 
 @application.route('/')
 def hello_world():
-    #resp = Response("Foo bar baz")
-    #resp.headers['Access-Control-Allow-Origin'] = '*'
-    return 'Hello'
+	#resp = Response("Foo bar baz")
+	#resp.headers['Access-Control-Allow-Origin'] = '*'
+	return 'Hello'
 
 @application.route('/aa')
-def hotels():
+def hotel_():
 	
 	sigStr = "%s%s%d" % (api_key,secret,int(time.time()))
 	signature = hashlib.sha256(sigStr.encode('utf-8')).hexdigest()
@@ -28,6 +28,54 @@ def hotels():
 	url='https://api.test.hotelbeds.com/hotel-content-api/1.0/hotels/2?language=CAT&useSecondaryLanguage=false'
 	
 	r = requests.get(url, headers=headers)
+	return r.text
+
+@application.route('/hotels')
+def hotels():
+	sigStr = "%s%s%d" % (api_key,secret,int(time.time()))
+	signature = hashlib.sha256(sigStr.encode('utf-8')).hexdigest()
+
+	headers= {
+		'Api-Key': api_key, 
+		'X-Signature': signature
+	}
+	
+	payload = {
+		"stay": {
+			"checkIn": "2019-06-08",
+			"checkOut": "2019-06-10"
+		},
+		"occupancies": [
+			{
+				"rooms": 1,
+				"adults": 2,
+				"children": 1,
+				"paxes": [
+					{
+						"type": "AD",
+						"age": 30
+					},
+					{
+						"type": "AD",
+						"age": 30
+					},
+					{
+						"type": "CH",
+						"age": 8
+					}
+				]
+			}
+		],
+		"hotels": {
+			"hotel": [
+				1067,1070,1075,135813,145214,1506,1508,1526,1533,1539,1550,161032,170542,182125,187939,212167,215417,228671,229318,23476
+				]
+	  }
+	}
+	
+	url='https://api.test.hotelbeds.com/hotel-api/1.0/hotels'
+	
+	r = requests.get(url, headers=headers, data=payload)
 	return r.text
 
 if __name__ == "__main__":
